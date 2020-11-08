c80306ff6b75edac8555be90874493fec6868e95
Mai Barishansky <maib@wix.com>
2020-11-08 09:31:18 +0200

    add unit tests and fix types

--- src/AddressInputOption/test/AddressInputOption.spec.js
+++ src/AddressInputOption/test/AddressInputOption.spec.js
@@ -5,9 +5,37 @@ import AddressInputOption from '../AddressInputOption';
 import { addressInputOptionPrivateDriverFactory } from './AddressInputOption.private.uni.driver';
 
 describe(AddressInputOption.displayName, () => {
+  const renderAddressInputOption = (props = {}) => (
+    <AddressInputOption {...props} />
+  );
   const render = createRendererWithUniDriver(
     addressInputOptionPrivateDriverFactory,
   );
 
   afterEach(cleanup);
+
+  it('should render', async () => {
+    const { driver } = render(renderAddressInputOption({}));
+
+    expect(await driver.exists()).toBe(true);
+  });
+
+  it('should trigger click callback', async () => {
+    const onClick = jest.fn();
+    const { driver } = render(renderAddressInputOption({ onClick }));
+
+    expect(onClick).not.toHaveBeenCalled();
+    await driver.click();
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it('should not trigger click callback when disabled', async () => {
+    const onClick = jest.fn();
+    const { driver } = render(
+      renderAddressInputOption({ disabled: true, onClick }),
+    );
+
+    await driver.click();
+    expect(onClick).not.toHaveBeenCalled();
+  });
 });
