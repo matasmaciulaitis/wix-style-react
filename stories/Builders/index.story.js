33ead70bef4d6fef1e177d457883ae8a6fbc2d3e
TalZa <talza@wix.com>
2020-11-22 14:18:57 +0200

    builders story

---
+++ stories/Builders/index.story.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  header,
+  tabs,
+  tab,
+  description,
+  importExample,
+  title,
+  divider,
+  example as baseExample,
+  playground,
+  api,
+  testkit,
+} from 'wix-storybook-utils/Sections';
+
+import { storySettings } from './storySettings';
+import allComponents from '../utils/allComponents';
+
+import DropdownLayout from '../../src/DropdownLayout';
+
+const example = config => baseExample({ components: allComponents, ...config });
+
+export default {
+  category: storySettings.category,
+  storyName: storySettings.storyName,
+
+  component: <DropdownLayout />,
+  componentPath: '../../src/DropdownLayout',
+
+  componentProps: {},
+
+  exampleProps: {
+    // Put here presets of props, for more info:
+    // https://github.com/wix/wix-ui/blob/master/packages/wix-storybook-utils/docs/usage.md#using-list
+  },
+
+  sections: [
+    header({}),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          description({
+            title: 'Description',
+            text:
+              'This line here should briefly describe component in just a sentence or two. It should be short and easy to read.',
+          }),
+
+          importExample(),
+
+          divider(),
+
+          title('Examples'),
+
+          example({
+            title: 'Simple Usage',
+            text: 'A simple example with compact preview',
+            source: '<<%= ComponentName %> buttonText="Hello World!"/>',
+          }),
+        ],
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
+  ],
+};
