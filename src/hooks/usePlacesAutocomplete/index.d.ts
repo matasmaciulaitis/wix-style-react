efb0ff8d149193b008823a2ca9e6d9f2ef9f878a
gendeld <danielge@wix.com>
2020-12-02 14:18:21 +0200

    Update types

--- src/hooks/usePlacesAutocomplete/index.d.ts
+++ src/hooks/usePlacesAutocomplete/index.d.ts
@@ -1,21 +1,24 @@
 import { AutocompleteClient } from './autocompleteClient';
 
-export type UpdatePredictions = (value: string) => void;
+export type UpdatePredictions<RequestOptions> = (
+  value: string,
+  requestOptions?: RequestOptions,
+) => void;
 
-export interface UsePlacesAutocompleteReturn<Prediction> {
+export interface UsePlacesAutocompleteReturn<Prediction, RequestOptions> {
   predictions: Prediction[];
   loading: boolean;
-  updatePredictions: UpdatePredictions;
+  updatePredictions: UpdatePredictions<RequestOptions>;
   clearPredictions: () => void;
 }
 export interface UsePlacesAutocompleteProps<Prediction, RequestOptions> {
   client: AutocompleteClient<Prediction, RequestOptions>;
-  debounce?: number;
-  defaultValue?: string;
+  debounceFn?: (callback: Function, debounceMs: number) => Function;
+  debounceMs?: number;
 }
 
 declare const usePlacesAutocomplete: <Prediction, RequestOptions>(
-  args?: UsePlacesAutocompleteProps<Prediction, RequestOptions>,
-) => UsePlacesAutocompleteReturn<Prediction>;
+  props?: UsePlacesAutocompleteProps<Prediction, RequestOptions>,
+) => UsePlacesAutocompleteReturn<Prediction, RequestOptions>;
 
 export default usePlacesAutocomplete;
