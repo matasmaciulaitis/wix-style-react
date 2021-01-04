a89c070c79eb99873ebdeb05746956b5df503833
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 17:44:53 +0200

    <Table/> - update table action cell example to function component

--- src/Table/docs/examples/TableActionCellExample.js
+++ src/Table/docs/examples/TableActionCellExample.js
@@ -1,65 +1,69 @@
 /* eslint-disable */
 
-class TableActionCellExample extends React.Component {
-  state = {
-    data: [
-      { member: 'Meghan Bishop', email: 'meghan.bishop@gmail.com' },
-      { member: 'Sara Porter', email: 's.porter@yahoo.com' },
-      { member: 'Deborah Rhodes', email: 'deborahmariarhodes@gmail.com' },
-      { member: 'Walter Jenning', email: 'walter.jenning@carrental.com' },
-    ],
-  };
+() => {
+  const data = [
+    { member: 'Meghan Bishop', email: 'meghan.bishop@gmail.com' },
+    { member: 'Sara Porter', email: 's.porter@yahoo.com' },
+    { member: 'Deborah Rhodes', email: 'deborahmariarhodes@gmail.com' },
+    { member: 'Walter Jenning', email: 'walter.jenning@carrental.com' },
+  ];
 
-  render() {
-    return (
-      <Table
-        data={this.state.data}
-        columns={[
-          { title: 'Member', render: row => row.member },
-          { title: 'Email', render: row => row.email },
-          {
-            title: '',
-            render: rowData => (
-              <TableActionCell
-                upgrade
-                primaryAction={{
-                  text: 'Edit',
-                  skin: 'standard',
-                  onClick: () => window.alert(`Editing data of member ${rowData.member}`),
-                }}
-                secondaryActions={[
-                  {
-                    text: 'Star',
-                    icon: <Icons.Star />,
-                    onClick: () => window.alert(`Starring data of member ${rowData.member}`),
-                  },
-                  {
-                    text: 'Duplicate',
-                    icon: <Icons.Duplicate />,
-                    onClick: () => window.alert(`Duplicating data of member ${rowData.member}`),
-                  },
-                  {
-                    text: 'Download',
-                    icon: <Icons.Download />,
-                    onClick: () => window.alert(`Downloading data of member ${rowData.member}`),
-                  },
-                  {
-                    text: 'Print',
-                    icon: <Icons.Print />,
-                    onClick: () => window.alert(`Printing data of member ${rowData.member}`),
-                  },
-                ]}
-                // How many actions are visible, whereas the rest would be shown within a popover menu
-                numOfVisibleSecondaryActions={2}
-              />
-            ),
-          },
-        ]}
-        onRowClick={(rowData, rowNum) => console.log(`Row data is: ${JSON.stringify(rowData)}. Row num is: ${rowNum}`)}
-      >
-        <Table.Content />
-      </Table>
-    );
-  }
-}
-export default TableActionCellExample
+  const columns = [
+    { title: 'Member', render: row => row.member },
+    { title: 'Email', render: row => row.email },
+    {
+      render: rowData => (
+        <TableActionCell
+          upgrade
+          primaryAction={{
+            text: 'Edit',
+            skin: 'standard',
+            onClick: () =>
+              window.alert(`Editing data of member ${rowData.member}`),
+          }}
+          secondaryActions={[
+            {
+              text: 'Star',
+              icon: <Icons.Star />,
+              onClick: () =>
+                window.alert(`Starring data of member ${rowData.member}`),
+            },
+            {
+              text: 'Duplicate',
+              icon: <Icons.Duplicate />,
+              onClick: () =>
+                window.alert(`Duplicating data of member ${rowData.member}`),
+            },
+            {
+              text: 'Download',
+              icon: <Icons.Download />,
+              onClick: () =>
+                window.alert(`Downloading data of member ${rowData.member}`),
+            },
+            {
+              text: 'Print',
+              icon: <Icons.Print />,
+              onClick: () =>
+                window.alert(`Printing data of member ${rowData.member}`),
+            },
+          ]}
+          numOfVisibleSecondaryActions={2}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      onRowClick={(rowData, rowNum) =>
+        console.log(
+          `Row data is: ${JSON.stringify(rowData)}. Row num is: ${rowNum}`,
+        )
+      }
+      data={data}
+      columns={columns}
+    >
+      <Table.Content />
+    </Table>
+  );
+};
