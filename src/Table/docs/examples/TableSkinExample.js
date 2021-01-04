4097297a1906deda3de27f08d01863b44063b34d
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 16:25:21 +0200

    <Table/> - update table skin example to function component

--- src/Table/docs/examples/TableSkinExample.js
+++ src/Table/docs/examples/TableSkinExample.js
@@ -1,38 +1,27 @@
 /* eslint-disable */
 
-class TableSkinExample extends React.Component {
-  state = {
-    data: [{ item: 'Item 1' }, { item: 'Item 2' }, { item: 'Item 3' }],
-  };
+() => {
+  const data = [{ item: 'Item 1' }, { item: 'Item 2' }, { item: 'Item 3' }];
+  const columns = [{ title: 'Column Name', render: row => row.item }];
 
-  render() {
-    return (
-      <Layout>
-        <Cell span={6}>
-          <Table
-            data={this.state.data}
-            skin="standard"
-            columns={[{ title: 'Column Name', render: row => row.item }]}
-          >
-            <Table.Content />
-          </Table>
-          <Box align="center" padding="small">
-            <Text size="small">Standard Skin</Text>
-          </Box>
-        </Cell>
-        <Cell span={6}>
-          <Table
-            data={this.state.data}
-            skin="neutral"
-            columns={[{ title: 'Column Name', render: row => row.item }]}
-          >
-            <Table.Content />
-          </Table>
-          <Box align="center" padding="small">
-            <Text size="small">Neutral Skin</Text>
-          </Box>
-        </Cell>
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <Cell span={6}>
+        <Table skin="standard" data={data} columns={columns}>
+          <Table.Content />
+        </Table>
+        <Box align="center" padding="small">
+          <Text size="small">Standard Skin</Text>
+        </Box>
+      </Cell>
+      <Cell span={6}>
+        <Table skin="neutral" data={data} columns={columns}>
+          <Table.Content />
+        </Table>
+        <Box align="center" padding="small">
+          <Text size="small">Neutral Skin</Text>
+        </Box>
+      </Cell>
+    </Layout>
+  );
+};
