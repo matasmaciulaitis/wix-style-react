5451466f0adf5be8294c1d4ce6842b9fb7eef58c
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 17:51:57 +0200

    <Table/> - update table aligned columns example to function component

--- src/Table/docs/examples/TableAlignedColumnsExample.js
+++ src/Table/docs/examples/TableAlignedColumnsExample.js
@@ -1,75 +1,52 @@
 /* eslint-disable */
 
-class TableAlignedColumnsExample extends React.Component {
-  state = {
-    data: [
-      { name: 'Red Slippers', sku: 25232564, inStock: true, price: '$14.00' },
-      { name: 'Velvet Hat', sku: 35246432, inStock: true, price: '$29.00' },
-      { name: 'Silver Jeans', sku: 4864310, inStock: false, price: '$69.00' },
-      { name: 'Orange Socks', sku: 125156422, inStock: true, price: '$7.00' },
-    ],
-  };
+() => {
+  const data = [
+    { name: 'Red Slippers', sku: 25232564, inStock: true, price: '$14.00' },
+    { name: 'Velvet Hat', sku: 35246432, inStock: true, price: '$29.00' },
+    { name: 'Silver Jeans', sku: 4864310, inStock: false, price: '$69.00' },
+    { name: 'Orange Socks', sku: 125156422, inStock: true, price: '$7.00' },
+  ];
 
-  render() {
-    return (
-      <Table
-        data={this.state.data}
-        columns={[
-          {
-            title: 'Name',
-            render: row => row.name,
-            width: '50%',
-          },
-          {
-            title: 'SKU',
-            render: row => row.sku,
-            width: '10%',
-            align: 'start',
-          },
-          {
-            title: 'Stock',
-            render: row =>
-              row.inStock ? (
-                <Badge size="small" skin="success">
-                  In Stock
-                </Badge>
-              ) : (
-                <Badge size="small" type="outlined">
-                  Out Of Stock
-                </Badge>
-              ),
-            width: '20%',
-            align: 'center',
-          },
-          {
-            title: 'Price',
-            render: row => row.price,
-            width: '10%',
-            align: 'end',
-          },
-          {
-            render: row => <TableActionCell secondaryActions={[{}]} />,
-            width: '10%',
-          },
-        ]}
-      >
-        <Table.Content />
-      </Table>
-    );
-  }
+  const InStock = ({ isInStock }) => (
+    <Badge size="small" skin={isInStock ? 'success' : 'outlined'}>
+      {isInStock ? 'In Stock' : 'Out Of Stock'}
+    </Badge>
+  );
 
-  _updateRow(rowNum, data) {
-    this.setState({
-      data: this.state.data.map((row, index) => {
-        if (index !== rowNum) {
-          return { ...row };
-        }
+  const columns = [
+    {
+      title: 'Name',
+      render: row => row.name,
+      width: '50%',
+    },
+    {
+      title: 'SKU',
+      render: row => row.sku,
+      width: '10%',
+      align: 'start',
+    },
+    {
+      title: 'Stock',
+      render: row => <InStock isInStock={row.inStock} />,
+      width: '20%',
+      align: 'center',
+    },
+    {
+      title: 'Price',
+      render: row => row.price,
+      width: '10%',
+      align: 'end',
+    },
+    {
+      render: row => <TableActionCell secondaryActions={[{}]} />,
+      width: '10%',
+    },
+  ];
 
-        return {
-          ...row,
-          ...data,
-        };
-      }),
-    });
-  }
-}
+  return (
+    <Table data={data} columns={columns}>
+      <Table.Content />
+    </Table>
+  );
+};
