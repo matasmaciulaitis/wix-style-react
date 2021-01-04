fa94da8ce237075dc541ae32c563ce4cf60a3687
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 17:23:16 +0200

    <Table/> - update table with unselectable rows example to function component

--- src/Table/docs/examples/TableWithUnselectableRowsExample.js
+++ src/Table/docs/examples/TableWithUnselectableRowsExample.js
@@ -1,28 +1,22 @@
 /* eslint-disable */
 
-class TableWithUnselectableRowsExample extends React.Component {
-  state = {
-    data: [
-      { category: 'All posts', posts: '123 posts', unselectable: true },
-      { category: 'Business', posts: '76 posts' },
-      { category: 'Culture', posts: '342 posts' },
-      { category: 'History', posts: '1024 posts' },
-    ],
-  };
+() => {
+  const data = [
+    { category: 'All posts', posts: '123 posts', unselectable: true },
+    { category: 'Business', posts: '76 posts' },
+    { category: 'Culture', posts: '342 posts' },
+    { category: 'History', posts: '1024 posts' },
+  ];
 
-  render() {
-    return (
-      <Card>
-        <Table
-          data={this.state.data}
-          columns={[
-            { title: 'Name', render: row => row.category },
-            { title: 'Type', render: row => row.posts },
-            { render: row => <TableActionCell secondaryActions={[{}]} /> },
-          ]}
-          showSelection
-        />
-      </Card>
-    );
-  }
-}
+  const columns = [
+    { title: 'Name', render: row => row.category },
+    { title: 'Type', render: row => row.posts },
+    { render: row => <TableActionCell secondaryActions={[{}]} /> },
+  ];
+
+  return (
+    <Card>
+      <Table data={data} columns={columns} showSelection />
+    </Card>
+  );
+};
