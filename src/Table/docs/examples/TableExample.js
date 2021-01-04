f5f26daa8ba0ade4bcd0c000cc937bc6176dfbdc
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 16:09:10 +0200

    <Table/> - update table structure example to function component

--- src/Table/docs/examples/TableExample.js
+++ src/Table/docs/examples/TableExample.js
@@ -1,33 +1,24 @@
 /* eslint-disable */
-class TableExample extends React.Component {
-  state = {
-    data: [
-      { item: 'Item 1' },
-      { item: 'Item 2' },
-      { item: 'Item 3' },
-      { item: 'Item 4' },
-    ],
-  };
 
-  render() {
-    return (
-      <Card>
-        <Table
-          data={this.state.data}
-          columns={[
-            { title: 'Column Name', render: row => row.item },
-            { title: 'Column Name', render: row => row.item },
-            { title: 'Column Name', render: row => row.item },
-            { title: 'Column Name', render: row => row.item },
-          ]}
-        >
-          <TableToolbar>
-            <TableToolbar.Title>Toolbar</TableToolbar.Title>
-          </TableToolbar>
-          <Table.SubToolbar>Sub Toolbar</Table.SubToolbar>
-          <Table.Content />
-        </Table>
-      </Card>
-    );
-  }
-}
+() => {
+  const data = [
+    { item: 'Item 1' },
+    { item: 'Item 2' },
+    { item: 'Item 3' },
+    { item: 'Item 4' },
+  ];
+
+  const columns = Array(4).fill( { title: 'Column Name', render: row => row.item });
+
+  return (
+    <Card>
+      <Table data={data} columns={columns}>
+        <TableToolbar>
+          <TableToolbar.Title>Toolbar</TableToolbar.Title>
+        </TableToolbar>
+        <Table.SubToolbar>Sub Toolbar</Table.SubToolbar>
+        <Table.Content />
+      </Table>
+    </Card>
+  );
+};
