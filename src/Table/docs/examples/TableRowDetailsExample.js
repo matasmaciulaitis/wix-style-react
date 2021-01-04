5226c8ee768156b58b5e54f3347ef14e36204c78
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 17:22:21 +0200

    <Table/> - update table row details example to function component

--- src/Table/docs/examples/TableRowDetailsExample.js
+++ src/Table/docs/examples/TableRowDetailsExample.js
@@ -1,26 +1,29 @@
 /* eslint-disable */
 
-class TableRowDetailsExample extends React.Component {
-  state = {
-    data: [
-      {
-        task: 'Upload New Collection Products',
-        date: '23 Sep 2020',
-        expandable: true,
-        expanded: false, 
-      },
-      { task: 'Q3 Sales Results Doc', date: '31 Oct 2020', expandable: false },
-      {
-        task: 'Staff Performace Reviews',
-        date: '12 Oct 2020',
-        expandable: true,
-        expanded: false,
-      },
-      { task: 'Budget Planning', date: '24 Nov 2020', expandable: true, expanded: false, },
-    ],
-  };
+() => {
+  const data = [
+    {
+      task: 'Upload New Collection Products',
+      date: '23 Sep 2020',
+      expandable: true,
+      expanded: false,
+    },
+    { task: 'Q3 Sales Results Doc', date: '31 Oct 2020', expandable: false },
+    {
+      task: 'Staff Performace Reviews',
+      date: '12 Oct 2020',
+      expandable: true,
+      expanded: false,
+    },
+    {
+      task: 'Budget Planning',
+      date: '24 Nov 2020',
+      expandable: true,
+      expanded: false,
+    },
+  ];
 
-  renderRowDetails(row) {
+  const renderRowDetails = row => {
     if (row.expandable) {
       return (
         <Box padding="20px">
@@ -30,34 +33,34 @@ class TableRowDetailsExample extends React.Component {
         </Box>
       );
     }
-  }
+  };
 
-  render() {
-    return (
-      <Table
-        data={this.state.data}
-        columns={[
-          { title: 'Task', render: row => row.task },
-          { title: 'Due Date', render: row => row.date },
-          {
-            render: row =>
-              row.expandable ? (
-                <TableActionCell
-                  primaryAction={{
-                    text: !row.expanded ? 'Expand' : 'Collapse',
-                    skin: 'inverted',
-                    onClick: row.expanded = !row.expanded,
-                  }}
-                />
-              ) : (
-                ''
-              ),
-          },
-        ]}
-        rowDetails={row => this.renderRowDetails(row)}
-      >
-        <Table.Content />
-      </Table>
-    );
-  }
-}
+  const columns = [
+    { title: 'Task', render: row => row.task },
+    { title: 'Due Date', render: row => row.date },
+    {
+      render: row =>
+        row.expandable ? (
+          <TableActionCell
+            primaryAction={{
+              text: !row.expanded ? 'Expand' : 'Collapse',
+              skin: 'inverted',
+              onClick: (row.expanded = !row.expanded),
+            }}
+          />
+        ) : (
+          ''
+        ),
+    },
+  ];
+  
+  return (
+    <Table
+      rowDetails={row => renderRowDetails(row)}
+      data={data}
+      columns={columns}
+    >
+      <Table.Content />
+    </Table>
+  );
+};
