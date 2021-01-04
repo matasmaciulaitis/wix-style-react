bcbbdc73a55e6985f69d0bff36cf8a649e758c08
Karolis Daugerdas <karolisd@wix.com>
2021-01-04 16:26:29 +0200

    <Table/> - update table row vertical padding example to function component

--- src/Table/docs/examples/TableRowVerticalPaddingExample.js
+++ src/Table/docs/examples/TableRowVerticalPaddingExample.js
@@ -1,33 +1,21 @@
 /* eslint-disable */
 
-class TableRowVerticalPaddingExample extends React.Component {
-  state = {
-    data: [{ item: 'Item 1' }, { item: 'Item 2' }, { item: 'Item 3' }],
-  };
-
-  render() {
-    return (
-      <Layout>
-        <Cell span={6}>
-          <Table
-            data={this.state.data}
-            columns={[{ title: 'Column Name', render: row => row.item }]}
-            rowVerticalPadding="medium"
-          >
-            <Table.Content />
-          </Table>
-        </Cell>
-        <Cell span={6}>
-          <Table
-            data={this.state.data}
-            columns={[{ title: 'Column Name', render: row => row.item }]}
-            rowVerticalPadding="large"
-          >
-            <Table.Content />
-          </Table>
-        </Cell>
-      </Layout>
-    );
-  }
-}
-
+() => {
+  const data = [{ item: 'Item 1' }, { item: 'Item 2' }, { item: 'Item 3' }];
+  const columns = [{ title: 'Column Name', render: row => row.item }];
+  
+  return (
+    <Layout>
+      <Cell span={6}>
+        <Table rowVerticalPadding="medium" data={data} columns={columns}>
+          <Table.Content />
+        </Table>
+      </Cell>
+      <Cell span={6}>
+        <Table rowVerticalPadding="large" data={data} columns={columns}>
+          <Table.Content />
+        </Table>
+      </Cell>
+    </Layout>
+  );
+};
