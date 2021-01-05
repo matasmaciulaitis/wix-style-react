2f655a6b7aaf9b02518a68962233823b414d99c2
Karolis Daugerdas <karolisd@wix.com>
2021-01-05 12:40:31 +0200

    <Table/> - update table bulk selection checkbox example to function component

--- src/Table/docs/examples/TableBulkSelectionCheckboxExample.js
+++ src/Table/docs/examples/TableBulkSelectionCheckboxExample.js
@@ -1,36 +1,41 @@
 /* eslint-disable */
 
-class TableBulkSelectionCheckboxExample extends React.Component {
-  state = {
-    data: [
-      {
-        name: 'Red Slippers',
-        sku: 25232564,
-        status: 'In Stock',
-        price: '$14.00',
-      },
-      {
-        name: 'Velvet Hat',
-        sku: 35246432,
-        status: 'In Stock',
-        price: '$29.00',
-      },
-      {
-        name: 'Silver Jeans',
-        sku: 4864310,
-        status: 'Out Of Stock',
-        price: '$69.00',
-      },
-      {
-        name: 'Orange Socks',
-        sku: 125156422,
-        status: 'In Stock',
-        price: '$7.00',
-      },
-    ],
-  };
+() => {
+  const data = [
+    {
+      name: 'Red Slippers',
+      sku: 25232564,
+      status: 'In Stock',
+      price: '$14.00',
+    },
+    {
+      name: 'Velvet Hat',
+      sku: 35246432,
+      status: 'In Stock',
+      price: '$29.00',
+    },
+    {
+      name: 'Silver Jeans',
+      sku: 4864310,
+      status: 'Out Of Stock',
+      price: '$69.00',
+    },
+    {
+      name: 'Orange Socks',
+      sku: 125156422,
+      status: 'In Stock',
+      price: '$7.00',
+    },
+  ];
 
-  getCheckboxContent(selectedCount, bulkSelectionState) {
+  const columns = [
+    { title: 'Name', render: row => row.name },
+    { title: 'SKU', render: row => row.sku },
+    { title: 'Status', render: row => row.status },
+    { title: 'Price', render: row => row.price },
+  ];
+
+  const getCheckboxContent = (selectedCount, bulkSelectionState) => {
     switch (bulkSelectionState) {
       case 'ALL':
         return `All Elements Selected`;
@@ -41,43 +46,33 @@ class TableBulkSelectionCheckboxExample extends React.Component {
           ? '1 Element Selected'
           : `${selectedCount} Elements Selected`;
     }
-  }
+  };
 
-  render() {
-    return (
-      <Card>
-        <Table
-          data={this.state.data}
-          columns={[
-            { title: 'Name', render: row => row.name },
-            { title: 'SKU', render: row => row.sku },
-            { title: 'Status', render: row => row.status },
-            { title: 'Price', render: row => row.price },
-          ]}
-          showSelection
-          hideBulkSelectionCheckbox
-        >
-          <Table.ToolbarContainer>
-            {({ selectedCount, bulkSelectionState }) => (
-              <TableToolbar>
-                <TableToolbar.ItemGroup position="start">
-                  <TableToolbar.Item>
-                    <Table.BulkSelectionCheckbox>
-                      <TableToolbar.SelectedCount>
-                        {this.getCheckboxContent(
-                          selectedCount,
-                          bulkSelectionState,
-                        )}
-                      </TableToolbar.SelectedCount>
-                    </Table.BulkSelectionCheckbox>
-                  </TableToolbar.Item>
-                </TableToolbar.ItemGroup>
-              </TableToolbar>
-            )}
-          </Table.ToolbarContainer>
-          <Table.Content />
-        </Table>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card>
+      <Table
+        showSelection
+        hideBulkSelectionCheckbox
+        data={data}
+        columns={columns}
+      >
+        <Table.ToolbarContainer>
+          {({ selectedCount, bulkSelectionState }) => (
+            <TableToolbar>
+              <TableToolbar.ItemGroup position="start">
+                <TableToolbar.Item>
+                  <Table.BulkSelectionCheckbox>
+                    <TableToolbar.SelectedCount>
+                      {getCheckboxContent(selectedCount, bulkSelectionState)}
+                    </TableToolbar.SelectedCount>
+                  </Table.BulkSelectionCheckbox>
+                </TableToolbar.Item>
+              </TableToolbar.ItemGroup>
+            </TableToolbar>
+          )}
+        </Table.ToolbarContainer>
+        <Table.Content />
+      </Table>
+    </Card>
+  );
+};
