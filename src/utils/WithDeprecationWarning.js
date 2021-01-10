55149f341d786bd8026880f312c87f734039a73b
Egozi <egozi@wix.com>
2021-01-10 15:51:10 +0200

    Deprecate old drivers

--- src/utils/WithDeprecationWarning.js
+++ src/utils/WithDeprecationWarning.js
@@ -1,15 +1,21 @@
 const path = require('path');
 
-let config = require(`${path.resolve(process.cwd())}/wsr.config.js`);
+let config;
 
-if (!config || !config.deprecationWarnings) {
+try {
+  config = require(`${path.resolve(process.cwd())}/wsr.config.js`);
+} catch (ex) {
   config = {
     deprecationWarnings: {},
   };
 }
 
 export const WithDeprecationWarning = driver => {
-  if (console && config.deprecationWarnings[driver.name] !== 'mute') {
+  if (
+    console &&
+    config.deprecationWarnings &&
+    config.deprecationWarnings[driver.name] !== 'mute'
+  ) {
     // eslint-disable-next-line no-console
     console.warn(`
     Warning! ${driver.name} is deprecated and will be removed.
