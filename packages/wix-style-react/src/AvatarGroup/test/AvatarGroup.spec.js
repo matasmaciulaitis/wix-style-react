734856ce31d086ee2dbeccaf9210eda97860c9f2
roibWix <roib@wix.com>
2021-01-19 14:07:07 +0200

    update spec

--- packages/wix-style-react/src/AvatarGroup/test/AvatarGroup.spec.js
+++ packages/wix-style-react/src/AvatarGroup/test/AvatarGroup.spec.js
@@ -5,31 +5,12 @@ import AvatarGroup from '../AvatarGroup';
 import { avatarGroupPrivateDriverFactory } from './AvatarGroup.private.uni.driver';
 
 describe(AvatarGroup.displayName, () => {
-  const render = createRendererWithUniDriver(
-    avatarGroupPrivateDriverFactory,
-  );
+  const render = createRendererWithUniDriver(avatarGroupPrivateDriverFactory);
 
   afterEach(cleanup);
 
   it('should render', async () => {
     const { driver } = render(<AvatarGroup />);
-
     expect(await driver.exists()).toBe(true);
-  });
-
-  it('should increment', async () => {
-    const { driver } = render(<AvatarGroup />);
-
-    await driver.clickButtonTimes(2);
-
-    expect(await driver.getCountText()).toEqual(
-      'You clicked this button even number (2) of times',
-    );
-  });
-
-  it('should allow changing the button text', async () => {
-    const { driver } = render(<AvatarGroup buttonText="Press me" />);
-
-    expect(await driver.getButtonText()).toEqual('Press me');
   });
 });
