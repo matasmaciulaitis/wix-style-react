031745ac920512917a1c596674650b9cd5d6cb89
Egozi <egozi@wix.com>
2021-01-26 15:00:10 +0200

    wsr lab

--- packages/wix-style-react/src/indexLab.d.ts
+++ packages/wix-style-react/src/indexLab.d.ts
@@ -2,4 +2,4 @@ export { default as TimeInput } from './TimeInput';
 export * from './TimeInput';
 
 export { default as Carousel } from './CarouselWIP';
-export * from './TimeInput';
+export * from './CarouselWIP';
