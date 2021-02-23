a9069832e6ed111215eb723212c628fb9337a6fd
Jonathan Adler <jonathana@wix.com>
2021-02-24 01:00:08 +0200

    Fix Popover and RadioGroup basic perf tests - remove classnames

--- packages/wix-ui-tpa/src/components/Popover/perf/PopoverBasic.tsx
+++ packages/wix-ui-tpa/src/components/Popover/perf/PopoverBasic.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { Popover, TriggerAction } from '../';
-import { classes } from './PopoverExtended.st.css';
 
 export const PopoverPerf = () => {
   return (
     <Popover
       placement={'top'}
-      className={classes.root}
       triggerAction={TriggerAction.hover}
     >
       <Popover.Element>This is the Popover.Element</Popover.Element>
--- packages/wix-ui-tpa/src/components/RadioButtonGroup/perf/RadioButtonGroupBasic.tsx
+++ packages/wix-ui-tpa/src/components/RadioButtonGroup/perf/RadioButtonGroupBasic.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { RadioButtonGroup } from '../';
 import { RadioButton } from '../../RadioButton';
-import { classes } from './RadioButtonGroupExtended.st.css';
 
 export const RadioButtonGroupPerf = () => {
   return (
     <RadioButtonGroup
-      className={classes.root}
       onChange={() => {}}
       name="Name"
       value={'1'}
