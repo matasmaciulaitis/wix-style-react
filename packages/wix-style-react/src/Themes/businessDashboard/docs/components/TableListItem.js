8e45635cb4d4f85374038dbee758b72fc782454f
Netanel Abekasis <netanela@wix.com>
2021-02-23 19:13:12 +0200

    review changes

--- packages/wix-style-react/src/Themes/businessDashboard/docs/components/TableListItem.js
+++ packages/wix-style-react/src/Themes/businessDashboard/docs/components/TableListItem.js
@@ -1,15 +1,20 @@
 export const playground = `
   <ThemeProvider theme={theme()}>
-    <Layout cols={1}>
-      <TableListItem
-        showDivider
-        options={[
-          { value: 'Personal Finance' },
-          { value: '7 posts' },
-          { value: 'Last update on 27 April 2020' },
-        ]}
-        onClick={() => { }}
-      />
-    </Layout>
+    <Card>
+      <Card.Content>
+        <Layout cols={1}>
+          <TableListItem
+            verticalPadding="small"
+            showDivider
+            options={[
+              { value: 'Personal Finance' },
+              { value: '7 posts' },
+              { value: 'Last update on 27 April 2020' },
+            ]}
+            onClick={() => { }}
+          />
+        </Layout>
+      </Card.Content>
+    </Card>
   </ThemeProvider>
 `;
